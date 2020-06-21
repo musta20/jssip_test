0c37e9d99dd5033128a2dafca238df9d77148beb
mustafa <must.g20@gmail.com>
2020-06-21 09:50:04 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Testme from './componant/testme';
+import Mainnav from "./componant/mainnav";
+import Sing from "./componant/Sing";
+import "./App.css";
 import * as serviceWorker from './serviceWorker';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   
+   <Router>
+      <div>
+        <nav>
+          <ul>
+       
+            <li>
+              <Link to="/">Testme</Link>
+            </li>
+            <li>
+              <Link to="/Sing">Sing</Link>
+            </li>
+            <li>
+              <Link to="/Mainnav">Mainnav</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        
+          <Route path="/Mainnav">
+            <Mainnav></Mainnav>
+          </Route>
+          
+          <Route path="/Sing">
+            <Sing></Sing>
+          </Route>
+          <Route path="/">
+           <Testme></Testme>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+ 
   </React.StrictMode>,
   document.getElementById('root')
 );
